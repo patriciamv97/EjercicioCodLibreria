5cb67d78627392f5b7ef6cf7c0114d29b7d83031
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-02-25 10:18:06 +0100

    Importamos la libreria

--- src/com/ejercicioscod/Main.java
+++ src/com/ejercicioscod/Main.java
@@ -1,8 +1,11 @@
 package com.ejercicioscod;
-
+import libreria.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Consola consola =new Consola("Hola");
+	consola.visualizar();
+	Ventana ventana = new Ventana("Adios");
+	ventana.visualizar();
     }
 }
