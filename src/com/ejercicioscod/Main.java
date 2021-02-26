bc7d6c8ebe888108b69da820ae5fa094bd78cbb3
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-02-26 09:51:07 +0100

    Mensajes impresos desde diferentes dispositivos.

--- src/com/ejercicioscod/Main.java
+++ src/com/ejercicioscod/Main.java
@@ -1,11 +1,31 @@
 package com.ejercicioscod;
-import libreria.*;
+
+//import libreria.Consola;
+import libreria.FactoriaDeSalidas;
+import libreria.InterfaceMetodosComunes;
+//import libreria.Ventana;
+
 public class Main {
 
     public static void main(String[] args) {
-	Consola consola =new Consola("Hola");
-	consola.visualizar();
-	Ventana ventana = new Ventana("Adios");
-	ventana.visualizar();
+
+        InterfaceMetodosComunes miSalida;
+        InterfaceMetodosComunes salidaVentana;
+        InterfaceMetodosComunes salidaImpresora;
+
+        miSalida = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA, "Visualización por consola completa");
+        miSalida.visualizar();
+
+        salidaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA, "Visualización por ventana completa");
+        salidaVentana.visualizar();
+
+        salidaImpresora=FactoriaDeSalidas.getProducto(FactoriaDeSalidas.IMPRESORA,"Impresión completa.");
+        salidaImpresora.visualizar();
+		/*
+		Consola consola = new Consola("Hola");
+        consola.visualizar();
+        Ventana ventana = new Ventana("Adios");
+        ventana.visualizar();
+        */
     }
 }
